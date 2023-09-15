47403935b8a029ac7b40df9003941ed6c52c2d8e
Jen Beiser MSFT <20113869+iamjenetzler@users.noreply.github.com>
2023-09-15 11:01:00 -0700

    updating workshop

--- Code/Databricks Notebooks/Lab 0 - Navigate Databricks Workspace/Lab 0 - Getting Around The Databricks Workspace.py
+++
@@ -1,9 +0,0 @@
-# Databricks notebook source
-# MAGIC %md
-# MAGIC #Working within the Databricks Workspace
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC 
-# MAGIC [Navigate the Databricks Workspace](https://learn.microsoft.com/en-us/azure/databricks/workspace/)
